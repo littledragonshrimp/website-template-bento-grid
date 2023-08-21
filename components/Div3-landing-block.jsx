c88b7a357f31748ecd8c25288ffb5125dfe81155
Jordan <JDLChang@gmail.com>
2023-08-21 14:53:46 -0500

    adds component landing block for testing

--- components/Div3-landing-block.jsx
+++ components/Div3-landing-block.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../src/Landing.scss'
 
-function Div3-landing - block() {
+function Div3LandingBlock() {
     return (
         <>
             <a className="div3 landing-block" href="#"><span className="block-text"><h2>Div 3 Name</h2><p>Get involved free for students and teachers</p></span><img src="../src/assets/images/forest.jpg" alt="" srcset="" /></a>
@@ -9,4 +9,4 @@ function Div3-landing - block() {
     )
 }
 
-export default Div3 - landing - block
+export default Div3LandingBlock
